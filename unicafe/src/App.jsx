6a04129ef34e5bc7f5337a071705acda8af2e844
edwinalbanez <edwinalbanez26@gmail.com>
2025-08-29 18:21:58 -0600

    1.11: unicafe, step 6

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
 const App = () => {
-  const [good, setGood] = useState(1);
+  const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
     <>
       <h1>Give feedback</h1>
-      <Button text="Good" />
-      <Button text="Neutral" />
-      <Button text="Bad" />
+      <Button text="Good" onClick={() => setGood(good + 1)}  />
+      <Button text="Neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="Bad" onClick={() => setBad(bad + 1)} />
 
       <Statistics stats={{ good, neutral, bad }} />
     </>
   );
 };
 
-const Button = ({text}) => <button> {text} </button>
+const Button = ({text, onClick}) => <button onClick={onClick}> {text} </button>
 
 const Statistics = ({stats}) => {
 
